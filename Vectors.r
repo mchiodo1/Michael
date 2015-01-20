6553e3403219454eab09b8aad144334495865a5c
Michael Chiodo <mike_3_34@hotmail.com>
2015-01-20 11:00:34 -0500

    Add Files

---
+++ Vectors.r
@@ -0,0 +1,15 @@
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+tf
+num_vect >= 6
+my_char <- c("My", "Name", "is")
+my_char <- c("My", "name", "is")
+my_char
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Michael Chiodo")
+my_name
+paste(my_name, collapse = " ")
+paste ("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+savehistory("~/SRT411/Swirl/part3.r")
