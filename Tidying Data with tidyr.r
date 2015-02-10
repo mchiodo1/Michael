ca8bd6145c77ddb8f775bab9cf3ddd58cddfbc64
mchiodo1 <mike_3_34@hotmail.com>
2015-02-10 10:04:33 -0500

    more swirl labs

---
+++ Tidying Data with tidyr.r
@@ -0,0 +1,20 @@
+library(tidyr)
+Every column contains a different variable
+students
+?gather
+gather(students, sex, count, -grade)
+students2
+res <- gather(students2, sex_class, count, -grade)
+res
+?separate
+separate(res, sex_class, c("sex", "class"))
+students3
+?spread
+extract_numeric("class5")
+students4
+passed
+failed
+passed <- passed %>% mutate(status = "passed")
+failed <- failed %>% mutate(status = "failed")
+bind_rows(passed, failed)
+sat
